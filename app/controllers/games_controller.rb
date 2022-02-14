333c77aa10cf64cc9d3947d54f373b1a55789f02
Charlotte MARION <marion.charlotte@orange.fr>
2022-02-14 15:51:19 +0100

    update

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0...10).map { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def score
+    @word = params[:response].upcase
+    @letters = params[:letters].split(" ").join
+    if include?(@word, @letters)
+      if english_word?(@word)
+        @response = "Congratulations! #{@word} is a valid English word!"
+      else
+        @response = "Sorry but #{@word} does not seem to be a valid English word..."
+      end
+    else
+      @response = "Sorry but #{@word} can't be built out of #{@letters}"
+    end
+  end
+
+  private
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def include?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+end
